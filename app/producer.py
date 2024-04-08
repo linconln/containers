7b1ed80a5d2d14afa57ec8383e714980e8cbfcf3
linconln <linconln@gmail.com>
2024-04-08 13:44:53 -0300

    2024-04-08-final

--- app/producer.py
+++ app/producer.py
@@ -1,3 +1,5 @@
+# Le o arquivo json e envia para o serviço de mensageria
+
 import datetime
 import pika
 import json
@@ -23,4 +25,52 @@ for transaction in transactions:
                           body=json.dumps(transaction)
                           )
 
-print("Finalizado")
+# verifica o cache, calcula a media, gera relatório no Min-IO quando encontra a condição de fraude e depois mostra os endereços para download dos relatórios
+
+from minio import Minio
+import redis
+import io
+
+cliente = Minio(
+    endpoint="nginx:9000", 
+    access_key="minioadmin", 
+    secret_key="minioadmin",
+    secure=False)
+
+bucket_name = "bucket"
+if cliente.bucket_exists(bucket_name):
+    print("Bucket existe!")
+else:
+    cliente.make_bucket(bucket_name)
+
+cache = redis.Redis(host='redis', port=6379, db=0)
+
+chaves = cache.keys("report*")
+
+for chave in chaves:
+    str_chave = chave.decode("utf-8")
+    str_chave = str_chave+".txt"
+    reports = cache.lrange(chave, 0, 999999)
+    value=""
+    size=0
+    for report in reports:
+        str_report=report.decode("utf-8")
+        value=value+str_report+"\n"
+
+    size=len(value)
+    value_as_bytes=value.encode("utf-8")
+    str_reports=io.BytesIO(value_as_bytes)
+
+    result = cliente.put_object(
+        bucket_name=bucket_name,
+        object_name=str_chave,
+        data=str_reports,
+        length=size
+    )
+
+    get_url = cliente.get_presigned_url(
+        method='GET',
+        bucket_name=bucket_name,
+        object_name= str_chave, )
+
+    print(f"Download URL: [GET] {get_url}")
